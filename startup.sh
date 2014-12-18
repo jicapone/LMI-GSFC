e0068b64bb56b8f606966c05039fa140149b2f2a
John Capone <jicapone@ananke.astro.umd.edu>
2014-12-18 13:35:16 -0500

    cosmetic changes to startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,14 @@
 #
 #!/bin/bash
 
-PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # allows startup.sh to be called from any directory.  will fail if last component of path to startup.sh is a symlink.
+# set project root directory
+export LMI_PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # allows startup.sh to be called from any directory.  will fail if last component of path to startup.sh is a symlink.
+
+# set lmi reduction path
+export REDUCTION="$LMI_PROJECT_ROOT/lmi_reduction/"
 
 # add pipeline directories to python path
-export PYTHONPATH=$PYTHONPATH:$PROJECT_ROOT/lmi_reduction:$PROJECT_ROOT/lmi_observing:$PROJECT_ROOT/lmi_photometry
+export PYTHONPATH=$LMI_PROJECT_ROOT/lmi_reduction:$LMI_PROJECT_ROOT/lmi_observing:$LMI_PROJECT_ROOT/lmi_photometry:$PYTHONPATH
+
+# set pipeline aliases
+alias cd_lmi='cd $LMI_PROJECT_ROOT'
