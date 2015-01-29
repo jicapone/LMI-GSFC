11d76013de643a6ede9a9a24861b14df6f072f7e
John Capone <jicapone@astro.umd.edu>
2015-01-29 12:07:21 -0500

    removed LMI from variable names

--- startup.sh
+++ startup.sh
@@ -5,25 +5,24 @@
 ##########################
 # check for dependencies #
 ##########################
-echo "Checking LMI pipeline dependencies:"
+echo "Checking pipeline add-on dependencies:"
 
-# check that RATIR pipeline has been sourced
-[ -z "$RAT_PROJECT_ROOT" ] && echo "Need to run RATIR pipeline's startup.sh first.  Aborting." && exit 1
-echo -e "\t* RATIR pipeline found:\n\t\t${RAT_PROJECT_ROOT}"
+# check that pipeline has been sourced
+[ -z "$PIPE_ROOT" ] && echo "Need to run the pipeline's startup.sh first.  Aborting." && exit 1
+echo -e "\t* Pipeline found:\n\t\t${PIPE_ROOT}"
 
 ######################
 # set up environment #
 ######################
-echo "Setting up LMI pipeline environment."
+echo "Setting up pipeline add-on environment."
 
 # set project root directory
-export LMI_PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # allows startup.sh to be called from any directory.  will fail if last component of path to startup.sh is a symlink.
+export PIPE_ADDON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # allows startup.sh to be called from any directory.  will fail if last component of path to startup.sh is a symlink.
 
 # set lmi reduction path
-export REDUCTION="$LMI_PROJECT_ROOT/lmi_reduction/"
+export REDUCTION="$PIPE_EXTRA_ROOT/lmi_reduction/"
 
 # add pipeline directories to python path
-export PYTHONPATH=$LMI_PROJECT_ROOT/lmi_reduction:$LMI_PROJECT_ROOT/lmi_observing:$LMI_PROJECT_ROOT/lmi_photometry:$PYTHONPATH
+export PYTHONPATH=$PIPE_EXTRA_ROOT/lmi_reduction:$PIPE_EXTRA_ROOT/lmi_observing:$PIPE_EXTRA_ROOT/lmi_photometry:$PYTHONPATH
 
-# set pipeline aliases
-alias cd_lmi='cd $LMI_PROJECT_ROOT'
+echo "Pipeline add-on ready."
