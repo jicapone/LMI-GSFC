8f7a979e6a99eae9be7a0832df5c221d88ed0d71
John Capone <jicapone@astro.umd.edu>
2015-08-24 20:28:16 -0400

    fixed bug in startup.sh

--- startup.sh
+++ startup.sh
@@ -20,9 +20,9 @@ echo "Setting up pipeline add-on environment."
 export PIPE_ADDON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # allows startup.sh to be called from any directory.  will fail if last component of path to startup.sh is a symlink.
 
 # set lmi reduction path
-export REDUCTION="$PIPE_EXTRA_ROOT/lmi_reduction/"
+export REDUCTION="$PIPE_ADDON_ROOT/lmi_reduction/"
 
 # add pipeline directories to python path
-export PYTHONPATH=$PIPE_EXTRA_ROOT/lmi_reduction:$PIPE_EXTRA_ROOT/lmi_observing:$PIPE_EXTRA_ROOT/lmi_photometry:$PYTHONPATH
+export PYTHONPATH=$PIPE_ADDON_ROOT/lmi_reduction:$PIPE_ADDON_ROOT/lmi_observing:$PIPE_ADDON_ROOT/lmi_photometry:$PYTHONPATH
 
 echo "Pipeline add-on ready."
